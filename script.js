975ad4f2f11b8a1d4f6e3363ef964e4c8ed93f92
Richet <shanon.richet@gmail.com>
2021-10-28 15:06:36 +0200

    new commit

--- script.js
+++ script.js
@@ -3,43 +3,29 @@ const section= document.querySelector('section')
 const button= document.querySelector('.submit')
 const eventName= document.querySelector('.eventName')
 const eventDate= document.querySelector('.eventDate')
-var eventList= document.querySelector('li')
 
 if(localStorage.getItem("nom") !== null){
     let eventList= document.createElement('li')
-    eventList.innerHTML= `${localStorage.getItem("nom") + localStorage.getItem("date")}`
-    let removeBtn= document.createElement('button')
-    removeBtn.innerHTML= 'X'
-    removeBtn.onclick= () => {
-        main.removeChild(eventList)
-        localStorage.removeItem('nom')  
-        localStorage.removeItem('date') 
-    }
-    eventList.appendChild(removeBtn)
-    main.appendChild(eventList)
+    eventList.innerHTML= `${localStorage.getItem("nom")}`
+main.appendChild(eventList)
 }
-const events= [{
-    name: eventName.value,
-    date: eventDate.value
-}]
 
-
+const events= []
+let i= 0;
+i< events.length;
 button.onclick= () => {
- let eventList= document.createElement('li')
- let removeBtn= document.createElement('button')
- let countDown= document.createElement('input')
- countDown.type= "time"
- removeBtn.innerHTML= "X"
- removeBtn.onclick= () => {
-     main.removeChild(eventList)
- }
- eventList.innerText= eventName.value + ' ' + eventDate.value;
- eventList.appendChild(removeBtn)
- eventList.appendChild(countDown)
- main.appendChild(eventList) 
- console.log(eventList)
-
- localStorage.setItem("nom", eventName.value)
- localStorage.setItem("date", eventDate.value)
+    let eventsArray= {
+        name: eventName.value,
+        date: eventDate.value
+    }
+    events.push(eventsArray)
+    console.log(eventsArray)
+    var list= document.createElement('li')
+    list.innerHTML= eventsArray.date + '-' + eventsArray.name;
+    main.appendChild(list)
+    localStorage.setItem('nom', JSON.stringify(events))
 }
+
+
+
 
