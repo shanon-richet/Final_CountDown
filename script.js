1352b32f141bcfd7484019e95fc4be8897dfa48b
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2021-11-18 08:44:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,33 @@ const section= document.querySelector('section')
 const button= document.querySelector('.submit')
 const eventName= document.querySelector('.eventName')
 const eventDate= document.querySelector('.eventDate')
+const eventTime= document.querySelector('.eventTime')
 
 let events = JSON.parse(localStorage.getItem('events'))
-
+const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+if (events === null) {
+    events = []
+}
+var months = 12
 button.onclick= () => {
-    let inputValue= eventName.value + eventDate.value
+    let date= new Date(eventDate.value)
+    let now= new Date().getTime();
+    var elapsed= date - now;
+    var days = Math.floor(elapsed / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((elapsed % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((elapsed % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((elapsed % (1000 * 60)) / 1000);
+
+    var time= days + "j " + hours + "h " + minutes + "m " + seconds + "s "
+
+    inputValue= eventName.value + ' / ' + date.toLocaleDateString('fr-FR', options) + ' / ' + eventTime.value + ' ' + time
     events.push(inputValue)
-    console.log(inputValue)
     let list= document.createElement('li')
     list.innerHTML= inputValue
     section.appendChild(list)
+
     localStorage.setItem('events', JSON.stringify(events))
+
 }
 
 for (let i= 0; i < events.length; i++) {
@@ -21,8 +37,4 @@ for (let i= 0; i < events.length; i++) {
     li.innerHTML= events[i]
     section.appendChild(li)
 }
-
-
-
-
 
